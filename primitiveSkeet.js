c2b4ac1fc2950e2856bc13b54c8b192d04602d47
SquigglySerg <serodrig@mymail.mines.edu>
2017-10-10 16:09:31 -0600

    Draw the background on the canvas.

--- primitiveSkeet.js
+++ primitiveSkeet.js
@@ -1,10 +1,21 @@
-var skeetGrid = matrix(10,10,0);
+var NUM_ROWS = 10;
+var NUM_COLS = 10;
+var skeetGrid = matrix(NUM_ROWS,NUM_COLS,0);
+var canvas;
+var ctx;
+var width;
+var height;
 
 $(document).ready(function(){
+    canvas = document.getElementById("skeetCanvas");
+    ctx = canvas.getContext("2d");
+    width = canvas.scrollWidth;
+    height = canvas.scrollHeight;
+    drawBackground();
 
     // Call the function update_scores() once when your game is loaded.
     //update_scores(); //Needs to be called for the High Scores API -- works?.
-    
+
     // Call the function highscore(score) when your game ends to submit a score. 
     //    Make sure to pass a score as an argument for the function.
 });
@@ -27,4 +38,20 @@ function matrix( rows, cols, defaultValue){
     }
 
     return M;
+}
+
+function drawBackground(){
+    //Sky
+    ctx.fillStyle = "Blue";
+    ctx.beginPath();
+    ctx.rect(0, 0, width, height);
+    ctx.stroke();
+    ctx.fill();
+    
+    //Floor
+    ctx.fillStyle = "Green";
+    ctx.beginPath();
+    ctx.rect(0, height * 4 / 5, width, height);
+    ctx.stroke();
+    ctx.fill();
 }
